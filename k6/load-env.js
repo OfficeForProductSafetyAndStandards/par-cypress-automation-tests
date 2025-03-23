0363895af061a3bec7f01dec71fc5b3dbcc1d156
Timothy Ajisafe <timothy.ajisafe@timztos.co.uk>
2025-03-23 12:46:33 +0000

    PVB-223 - wip

--- k6/load-env.js
+++ k6/load-env.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const fs = require('fs');
 
-const envFile = `.env.${process.env.TEST_ENV || 'test'}`;
+const envFile = `.env.${process.env.TEST_ENV || 'local'}`;
 const envConfig = dotenv.parse(fs.readFileSync(envFile));
 
 for (const k in envConfig) {
