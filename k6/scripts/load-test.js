38013c84f09908505568f098f29474c24e5662c3
Timothy Ajisafe <timothy.ajisafe@timztos.co.uk>
2025-03-23 13:25:15 +0000

    PVB-223 - wip

--- k6/scripts/load-test.js
+++ k6/scripts/load-test.js
@@ -6,7 +6,7 @@ export const options = {
     vus: 5,
     duration: '30s',
     thresholds: {
-        http_req_duration: ['p(95)<2000'], // 95% of requests must be under 1200ms
+        http_req_duration: ['p(95)<5000'], // 95% of requests must be under 1200ms
     },
 };
 
